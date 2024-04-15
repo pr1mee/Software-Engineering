3578e42815edd5807bc8d5715938a49dca4a18b4
prime <dev@pr1me.dev>
2024-04-15 14:48:41 +0200

    Klammern hinzugefügt, für bessere Übersicht

--- src/main/java/org/hbrs/se1/ss24/uebung1/businesslogic/AbstractNumberTransformer.java
+++ src/main/java/org/hbrs/se1/ss24/uebung1/businesslogic/AbstractNumberTransformer.java
@@ -7,8 +7,8 @@ public abstract class AbstractNumberTransformer implements NumberTransformer {
 
     @Override
     public final String transformNumber(int number) {
-        return number < MIN || number > MAX //Wertebereich
-                ? "Die Zahl befindet sich ausserhalb des maximalen Wertebereichs"
+        return (number < MIN || number > MAX) //Wertebereich
+                ? "Die Zahl befindet sich ausserhalb des maximalen Wertebereichs ]1:3000["
                 : transform(number);
     }
 
