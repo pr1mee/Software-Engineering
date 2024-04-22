3798193c715edc0797f7e48124abeece831c68f2
prime <dev@pr1me.dev>
2024-04-22 21:01:30 +0200

    Restructured directories and pre FA FA1

--- src/main/java/org/hbrs/se1/ss24/uebung2/CardBox.java
+++ src/main/java/org/hbrs/se1/ss24/uebung2/CardBox.java
@@ -1,6 +1,18 @@
 package org.hbrs.se1.ss24.uebung2;
 
-public class CardBox {
-    private Iterable<PersonCard> personCards;
+import java.util.Collection;
 
+public class CardBox {
+    private Collection<PersonCard> personCards;
+    public void addPersonCard(PersonCard personCard) throws CardBoxException {
+        if(containsID(personCard.getId()))
+            throw new CardBoxException(personCard.getId());
+        personCards.add(personCard);
+    }
+    private boolean containsID(int id){
+        for(PersonCard personCard : personCards){
+            if(personCard.getId().equals(id))return true;
+        }
+        return false;
+    }
 }
---
+++ src/main/java/org/hbrs/se1/ss24/uebung2/CardBoxException.java
@@ -0,0 +1,7 @@
+package org.hbrs.se1.ss24.uebung2;
+
+public class CardBoxException extends Exception{
+    public CardBoxException(int ms) {
+        super("Das CardBox-Objekt mit der ID " + ms + " ist bereits vorhanden.");
+    }
+}
