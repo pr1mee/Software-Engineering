70233ff52950ec243fd03adc66ff8872a6e3ebdc
prime <dev@pr1me.dev>
2024-04-23 13:32:02 +0200

    FA2

--- src/main/java/org/hbrs/se1/ss24/uebung2/CardBox.java
+++ src/main/java/org/hbrs/se1/ss24/uebung2/CardBox.java
@@ -1,6 +1,7 @@
 package org.hbrs.se1.ss24.uebung2;
 
 import java.util.Collection;
+import java.util.function.Predicate;
 
 public class CardBox {
     private Collection<PersonCard> personCards;
@@ -9,14 +10,23 @@ public class CardBox {
             throw new CardBoxException(personCard.getId());
         personCards.add(personCard);
     }
-    public String deletePersonCard(int id) {
-        return "";
+    public String deletePersonCard(int id) throws Exception {
+        if(personCards.removeIf(new Predicate<PersonCard>() {
+            @Override
+            public boolean test(PersonCard personCard) {
+                return personCard.getId() == id;
+            }
+        })) return "";
+        return "Fehler: PersonCard ID nicht gefunden"; //TODO
     }
     private boolean containsID(int id){
-        for(PersonCard personCard : personCards){
-            if(id == personCard.getId())
-                return true;
-        }
-        return false;
+        return personCards.stream().anyMatch(new Predicate<PersonCard>() {
+
+            @Override
+            public boolean test(PersonCard personCard) {
+
+                return personCard.getId()==id;
+            }
+        });
     }
 }
